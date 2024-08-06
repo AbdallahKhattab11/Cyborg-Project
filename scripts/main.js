8c987a47f1f5adacb9bfbaae119d9eb3f1ef7100
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-06 05:40:22 +0300

    fix: rest paths & perf: refactor main.js code

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,37 @@
-'use strict'
+"use strict";
 
-
-// Shared Content
+// Global variables
+const LOADING_PAGE_TIME_MS = 500;
+let currentPage = window.location.pathname;
 
 // Shared variables
-let animation = document.querySelector(".animation");
-let a = document.querySelectorAll("nav .container .links ul li a"); // navbar links
-let barsIcon = document.getElementById("bars-icon");
-let linksDiv = document.querySelector("nav .container .links");
+const loadingPageEle = document.querySelector(".animation");
+const navBarContainer = document.querySelector("nav .container .links");
+const navBarLinks = document.querySelectorAll("nav .container .links ul li a");
+const barsIcon = document.getElementById("bars-icon");
 
 //* ------------------------------------------
 
 // Home variables
-let bannerBtn = document.querySelector(".banner .banner-btn");
-let gamingLibraryBtns = document.querySelectorAll("section .gaming-library .items .item ul li:last-child a");
-let discoverPopularBtn = document.querySelector(".mostPopular .btn a");
-let viewYourLibraryBtn = document.querySelector(".gaming-library .btn a");
+const bannerBtn = document.querySelector(".banner .banner-btn");
+const gamingLibraryBtns = document.querySelectorAll(
+  "section .gaming-library .items .item ul li:last-child a"
+);
+const discoverPopularBtn = document.querySelector(".mostPopular .btn a");
+const viewYourLibraryBtn = document.querySelector(".gaming-library .btn a");
 
 //* ------------------------------------------
 
 // browse variables
-let viewAllGamesLink = document.querySelector(".top-downloaded .top-downloaded-link"); 
-let goToProfileBtn = document.querySelector(".how-to-start-live .btn-goToProfile a");
-let discoverAllStreamsBtn = document.querySelector(".most-popular-live-stream .discover-all-streams-btn a");
+const viewAllGamesLink = document.querySelector(
+  ".top-downloaded .top-downloaded-link"
+);
+const goToProfileBtn = document.querySelector(
+  ".how-to-start-live .btn-goToProfile a"
+);
+const discoverAllStreamsBtn = document.querySelector(
+  ".most-popular-live-stream .discover-all-streams-btn a"
+);
 
 //* ------------------------------------------
 
@@ -31,165 +40,170 @@ let discoverAllStreamsBtn = document.querySelector(".most-popular-live-stream .d
 //* ------------------------------------------
 
 // Streams variables
-let loadMoreStreamsBtn = document.querySelector(".most-popular-live-stream .load-more-streamsBtn a");
+const loadMoreStreamsBtn = document.querySelector(
+  ".most-popular-live-stream .load-more-streamsBtn a"
+);
 
 //* ------------------------------------------
 
 // Profile variables
-let loadMoreClipsBtn = document.querySelector(".most-popular-clips .load-more-clips p");
-
-
+const loadMoreClipsBtn = document.querySelector(
+  ".most-popular-clips .load-more-clips p"
+);
 
 //! ----------Start of Shared functions--------------------------------------------------------
 
 //* --------------loading animation------------------------
 
-document.addEventListener("DOMContentLoaded", function () {
-  animation.style.cssText = " display: flex;";
-});
-document.addEventListener("DOMContentLoaded", function () {
+function loadingPageAnimation() {
+  loadingPageEle.style.cssText = "display: flex;";
+
   setTimeout(() => {
-    animation.style.cssText = " display: none;";
-  }, 500);
-});
+    loadingPageEle.style.cssText = "display: none;";
+  }, LOADING_PAGE_TIME_MS);
+}
 
-
+loadingPageAnimation();
 
 //* ------------------------------------------
 
 // add clicked link to session storage
-a.forEach((link) => {
+navBarLinks.forEach((link) => {
   link.addEventListener("click", function () {
     sessionStorage.setItem("activeLink", this.href);
   });
 });
 
-
 // set active class to active link based on session storage
 function getActiveLink() {
   const activeLink = sessionStorage.getItem("activeLink");
-  if (activeLink) {
-    a.forEach((link) => {
-      link.classList.remove("active");
-      if (link.href === activeLink) {
-        link.classList.add("active");
-      }
-    });
-  }
+
+  if (!activeLink) return;
+
+  navBarLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.href === activeLink) link.classList.add("active");
+  });
 }
-document.addEventListener("DOMContentLoaded", function () {
-  getActiveLink();
-});
+document.addEventListener("DOMContentLoaded", getActiveLink);
 
 //* ------------------------------------------
 
-  // * add active class to links div
-  barsIcon.addEventListener("click", function () {
-    linksDiv.classList.toggle("active");
-    // change icon
-    if (linksDiv.classList.contains("active")) {
-      barsIcon.className = "fa-solid fa-xmark";
-    } else {
-      barsIcon.className = "fa-solid fa-bars";
-    }
-  });
+// * add active class to links div
+function toggleNavMenu() {
+  const hasActiveClass = navBarContainer.classList.contains("active");
+  const iconClass = hasActiveClass ? "bars" : "xmark";
 
+  navBarContainer.classList.toggle("active");
+  barsIcon.className = `fa-solid fa-${iconClass}`;
+}
+
+barsIcon.addEventListener("click", toggleNavMenu);
 
 //! ----------End of Shared functions--------------------------------------------------------
 
-// get the current page.
-let currentPage = window.location.pathname;
+// Load pages scripts
+const isHomePage = currentPage === "/index.html";
+const isBrowsePage = currentPage === "/browse.html";
+const isStreamsPage = currentPage === "/streams.html";
 
-document.addEventListener("DOMContentLoaded", function () {
-  console.log(currentPage);
-  
-  if (currentPage === "/index.html" || currentPage === "/Cyborg-Project/index.html" || currentPage === "/Cyborg-Project/") {
+if (isHomePage) {
+  homePageScripts();
+  return;
+}
 
-    //* ----------handle banner Btn----------------------------
-    bannerBtn.addEventListener("click", function () {
-      sessionStorage.setItem("activeLink", this.href);
-    });
-    document.addEventListener("DOMContentLoaded", function () {
-      getActiveLink();
-    });
-  
-    //* ----------handle discover btn----------------------------
-    discoverPopularBtn.addEventListener("click", function () {
-      sessionStorage.setItem("activeLink", this.href);
-    });
-    document.addEventListener("DOMContentLoaded", function () {
-      getActiveLink();
-    });
-  
-    // add active to download btn in gaming library
-    gamingLibraryBtns.forEach((element) => {
-      element.onclick = function (el) {
-        el.preventDefault();
-        this.classList.toggle("active");
-        gamingLibraryBtns.forEach((e) => {
-          if (e.classList.contains("active")) {
-            e.textContent = "downloaded";
-          } else {
-            e.textContent = "download";
-          }
-        });
-      };
-    });
-  
-    //* ----------handle view your library btn----------------------------
-    viewYourLibraryBtn.addEventListener("click", function () {
-      sessionStorage.setItem("activeLink", this.href);
-    });
-    document.addEventListener("DOMContentLoaded", function () {
-      getActiveLink();
-    });
+if (isBrowsePage) {
+  browsePageScripts();
+  return;
+}
 
+if (isStreamsPage) {
+  streamsPageScripts();
+}
 
+// Pages scripts
+function homePageScripts() {
+  //* ----------handle banner Btn----------------------------
+  bannerBtn.addEventListener("click", function () {
+    sessionStorage.setItem("activeLink", this.href);
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    getActiveLink();
+  });
 
+  //* ----------handle discover btn----------------------------
+  discoverPopularBtn.addEventListener("click", function () {
+    sessionStorage.setItem("activeLink", this.href);
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    getActiveLink();
+  });
 
+  // add active to download btn in gaming library
+  gamingLibraryBtns.forEach((element) => {
+    element.onclick = function (el) {
+      el.preventDefault();
+      this.classList.toggle("active");
+      gamingLibraryBtns.forEach((e) => {
+        if (e.classList.contains("active")) {
+          e.textContent = "downloaded";
+        } else {
+          e.textContent = "download";
+        }
+      });
+    };
+  });
 
-  }else if (currentPage === "/browse.html" || currentPage === "/Cyborg-Project/browse.html") {
+  //* ----------handle view your library btn----------------------------
+  viewYourLibraryBtn.addEventListener("click", function () {
+    sessionStorage.setItem("activeLink", this.href);
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    getActiveLink();
+  });
+}
 
-//* ----------handle swiper.js----------------------------
-    const swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      loop: true,
+function browsePageScripts() {
+  //* ----------handle swiper.js----------------------------
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
 
-      grabCursor: true,
+    grabCursor: true,
 
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      300: {
+        slidesPerView: 1,
+        spaceBetween: 20,
       },
-
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
       },
-      breakpoints: {
-        300: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-        992: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
       },
-    });
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
 
   //* ----------handle view all games Btn----------------------------
   viewAllGamesLink.addEventListener("click", function () {
@@ -197,69 +211,65 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   document.addEventListener("DOMContentLoaded", getActiveLink());
 
-//* ----------handle go to profile Btn----------------------------
+  //* ----------handle go to profile Btn----------------------------
   goToProfileBtn.addEventListener("click", function () {
     sessionStorage.setItem("activeLink", this.href);
   });
   document.addEventListener("DOMContentLoaded", getActiveLink());
 
-//* ----------handle discover all streams Btn----------------------------
+  //* ----------handle discover all streams Btn----------------------------
   discoverAllStreamsBtn.addEventListener("click", function () {
     sessionStorage.setItem("activeLink", this.href);
   });
   document.addEventListener("DOMContentLoaded", getActiveLink());
+}
 
-  }else if (currentPage === "/details.html" || currentPage === "/Cyborg-Project/details.html") {
+function streamsPageScripts() {
+  //* ----------handle swiper.js----------------------------
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
 
-  }else if (currentPage === "/streams.html" || currentPage === "/Cyborg-Project/streams.html") {
+    grabCursor: true,
 
-    //* ----------handle swiper.js----------------------------
-    const swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
 
-      grabCursor: true,
-
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      300: {
+        slidesPerView: 1,
+        spaceBetween: 20,
       },
-
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
       },
-      breakpoints: {
-        300: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-        992: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
       },
-    });
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
 
-    //* ----------handle load more streams Btn----------------------------
-    loadMoreStreamsBtn.addEventListener("click", function () {
-      sessionStorage.setItem("activeLink", this.href);
-    });
-    document.addEventListener("DOMContentLoaded", getActiveLink());
-  }else {
-    // Profile page
-  }
-})
+  //* ----------handle load more streams Btn----------------------------
+  loadMoreStreamsBtn.addEventListener("click", function () {
+    sessionStorage.setItem("activeLink", this.href);
+  });
+
+  getActiveLink();
+}
